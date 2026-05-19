e263de6c1407d095a7a834fa9bbe5ee3ffde328c
Shiksha Agrawal <shikshaagrawal352@gmail.com>
2026-05-19 10:10:35 +0530

    Updation

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const app = express();
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: "*",
+    origin: "https://complaint-management-system-frontend-wiov.onrender.com",
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
